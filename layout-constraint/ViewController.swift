89a447f6a011c0844d1c77cd80d7c2b7becf561a
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2022-10-09 00:30:58 +0100

    connecting tableView with datasource

--- layout-constraint/ViewController.swift
+++ layout-constraint/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var viagensTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        viagensTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
 }
 
