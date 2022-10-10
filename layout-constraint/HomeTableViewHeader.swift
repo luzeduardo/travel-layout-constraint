35e95dc3c5984ab67b248d7bf12f4a5d506663f6
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2022-10-10 23:12:05 +0100

    rounding banner

--- layout-constraint/HomeTableViewHeader.swift
+++ layout-constraint/HomeTableViewHeader.swift
@@ -13,8 +13,11 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var bannerView: UIView!
 
     func configuraView() {
         headerView.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
     }
 }
