e69ca6e2756368bb32f1fa70749586e1f71bb08f
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2022-10-11 08:00:34 +0100

    styling custom cell view with constraint positioning

---
+++ layout-constraint/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  layout-constraint
+//
+//  Created by eduardo.luz on 11/10/2022.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
