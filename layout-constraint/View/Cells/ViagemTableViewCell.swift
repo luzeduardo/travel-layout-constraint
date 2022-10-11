455e6302c4392008b4dc9bfa26a45096d66c0916
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2022-10-11 22:45:18 +0100

    assigning data to labels

--- layout-constraint/View/Cells/ViagemTableViewCell.swift
+++ layout-constraint/View/Cells/ViagemTableViewCell.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class ViagemTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    //MARK: - IBOutlets
+    @IBOutlet weak var backgroundViewCell: UIView!
+    @IBOutlet weak var viagemImage: UIImageView!
+    @IBOutlet weak var tituloViagemLabel: UILabel!
+    @IBOutlet weak var subtituloViagemLabel: UILabel!
     
+    @IBOutlet weak var diariaViagemLabel: UILabel!
+    @IBOutlet weak var precoViagemLabel: UILabel!
+    @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
+    @IBOutlet weak var precoSemDescontoLabel: UILabel!
+    
+    func configuraCelular(_ viagem: Viagem?) {
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0)"
+        
+        //adding striked text to label
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(string: "R$ \(viagem?.precoSemDesconto ?? 0)")
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numeroDeDias = viagem?.diaria, let numeroDeHospedes = viagem?.hospedes {
+            let diarias = numeroDeDias == 1 ? "Diária" : "Diárias"
+            let hospedes = numeroDeHospedes == 1 ? "Hospede" : "Hospedes"
+            diariaViagemLabel.text = "\(numeroDeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+    }
 }
--- layout-constraint/ViewController.swift
+++ layout-constraint/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return sessaoDeViagens?[section].numeroDeLinhas ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
